b8fb7b569e48d651e586b900d3d4bad1676cbb33
pyjong1999 <pyjong1999@gmail.com>
2025-12-30 01:28:48 +0900

    DB Genesis consistency update & init.rs

--- database/src/immemorydb.rs
+++ database/src/immemorydb.rs
@@ -1,9 +1,17 @@
-use std::{collections::{BTreeMap, HashMap}, sync::Arc};
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::Arc,
+};
 
-use parking_lot::{RwLock};
-use primitives::{block::{Block, Header}, transaction::SignedTransaction, types::{Account, Address, U256}, world::World};
+use parking_lot::RwLock;
+use primitives::{
+    block::{Block, Header},
+    transaction::SignedTransaction,
+    types::{Account, Address},
+    world::World,
+};
 
-use crate::{error::DatabaseError, traits::DatabaseTrait};
+use crate::{error::DatabaseError, genesis::genesis_accounts_info, traits::DatabaseTrait};
 
 #[derive(Debug)]
 pub struct InMemoryDB {
@@ -14,29 +22,20 @@ pub struct InMemoryDB {
 }
 
 impl InMemoryDB {
-
-    // 28dcb1338b900419cd613a8fb273ae36e7ec2b1d pint
-    // 0534501c34f5a0f3fa43dc5d78e619be7edfa21a chain
-    // 08041f667c366ee714d6cbefe2a8477ad7488f10 apple
-    // b2aaaf07a29937c3b833dca1c9659d98a9569070 banana
+    // Addr: 28dcb1338b900419cd613a8fb273ae36e7ec2b1d, Seed: pint
+    // Addr: 0534501c34f5a0f3fa43dc5d78e619be7edfa21a, Seed: chain
     pub fn genesis_state() -> Self {
         let mut db = Self::new();
-        let account = Account {
-            nonce: 0,
-            balance: U256::from(1000000000),
-        };
-        let address = Address::from_hex("28dcb1338b900419cd613a8fb273ae36e7ec2b1d".to_string()).unwrap();
-        db.add_account(address, account.clone()).unwrap();
-        let address = Address::from_hex("0534501c34f5a0f3fa43dc5d78e619be7edfa21a".to_string()).unwrap();
-        db.add_account(address, account.clone()).unwrap();
+        for (address, account) in genesis_accounts_info() {
+            db.add_account(address, account).unwrap();
+        }
         db
     }
-
 
     pub fn new() -> Self {
         let mut accounts: BTreeMap<u64, HashMap<Address, Account>> = BTreeMap::new();
         accounts.insert(0 as u64, HashMap::new());
-        
+
         let mut field: BTreeMap<u64, World> = BTreeMap::new();
         field.insert(0, World::new());
 
@@ -45,19 +44,23 @@ impl InMemoryDB {
         blockchain.insert(0, genesis_block);
 
         Self {
-            accounts: RwLock::new(accounts), 
-            field: RwLock::new(field), 
+            accounts: RwLock::new(accounts),
+            field: RwLock::new(field),
             blockchain: RwLock::new(blockchain),
             latest: RwLock::new(0),
         }
     }
 
-    pub fn add_account(&mut self, address: Address, account: Account) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn add_account(
+        &mut self,
+        address: Address,
+        account: Account,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let mut state = self.accounts.write();
 
         let latest_accounts = state.entry(*self.latest.read()).or_default();
         latest_accounts.insert(address, account);
-        
+
         Ok(())
     }
 }
@@ -66,15 +69,20 @@ impl DatabaseTrait for Arc<InMemoryDB> {
     fn latest_block_number(&self) -> u64 {
         *self.latest.read()
     }
-    
+
     fn basic(&self, address: &Address) -> Result<Option<Account>, Box<dyn std::error::Error>> {
         let mut state = self.accounts.write();
 
-        let latest_accounts: &mut HashMap<Address, Account> = state.entry(self.latest_block_number()).or_default();
+        let latest_accounts: &mut HashMap<Address, Account> =
+            state.entry(self.latest_block_number()).or_default();
         Ok(latest_accounts.get(address).or(None).cloned())
     }
-    
-    fn get_state(&self, block_no: u64) -> Result<(Option<HashMap<Address, Account>>, Option<World>), Box<dyn std::error::Error>> {
+
+    fn get_state(
+        &self,
+        block_no: u64,
+    ) -> Result<(Option<HashMap<Address, Account>>, Option<World>), Box<dyn std::error::Error>>
+    {
         let accounts = self.accounts.read();
         let mut account_base = None;
         if let Some(state_account) = accounts.get(&block_no) {
@@ -90,7 +98,10 @@ impl DatabaseTrait for Arc<InMemoryDB> {
         Ok((account_base, field_base))
     }
 
-    fn get_block(&self, block_no: u64) -> Result<Option<Block>, Box<dyn std::error::Error + 'static>> {
+    fn get_block(
+        &self,
+        block_no: u64,
+    ) -> Result<Option<Block>, Box<dyn std::error::Error + 'static>> {
         let blockchain = self.blockchain.read();
         if let Some(block) = blockchain.get(&block_no) {
             Ok(Some(block.clone()))
@@ -99,18 +110,24 @@ impl DatabaseTrait for Arc<InMemoryDB> {
         }
     }
 
-    fn get_block_by_hash(&self, hash: primitives::types::BlockHash) -> Result<Option<Block>, Box<dyn std::error::Error + 'static>> {
+    fn get_block_by_hash(
+        &self,
+        hash: primitives::types::BlockHash,
+    ) -> Result<Option<Block>, Box<dyn std::error::Error + 'static>> {
         let blockchain = self.blockchain.read();
         for (_heignt, block) in blockchain.iter() {
             let tmp_hash = block.header().calculate_hash();
             if hash == tmp_hash {
-                return Ok(Some(block.clone()))
+                return Ok(Some(block.clone()));
             }
         }
         Err(Box::new(DatabaseError::DataNotExists))
     }
 
-    fn get_header(&self, block_no: u64) -> Result<Option<Header>, Box<dyn std::error::Error + 'static>> {
+    fn get_header(
+        &self,
+        block_no: u64,
+    ) -> Result<Option<Header>, Box<dyn std::error::Error + 'static>> {
         let blockchain = self.blockchain.read();
         if let Some(block) = blockchain.get(&block_no) {
             Ok(Some(block.header().clone()))
@@ -125,9 +142,13 @@ impl DatabaseTrait for Arc<InMemoryDB> {
         let block = blockchain.get(&latest).unwrap();
         block.header.clone()
     }
-    
-    fn update(&self, new_account_state: HashMap<Address, Account>, new_field_state: World, block: Block) 
-        -> Result<(), Box<dyn std::error::Error>>{
+
+    fn update(
+        &self,
+        new_account_state: HashMap<Address, Account>,
+        new_field_state: World,
+        block: Block,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let mut latest = self.latest.write();
         *latest += 1;
         let mut state = self.accounts.write();
@@ -142,7 +163,7 @@ impl DatabaseTrait for Arc<InMemoryDB> {
 
         Ok(())
     }
-    
+
     fn remove_data(&self, height: u64) -> Result<(), Box<dyn std::error::Error>> {
         let latest = self.latest.read();
         if *latest != height {
@@ -161,8 +182,11 @@ impl DatabaseTrait for Arc<InMemoryDB> {
 
         Ok(())
     }
-    
-    fn get_transaction_by_hash(&self, hash: primitives::types::TxHash) -> Result<Option<(SignedTransaction, u64)>, Box<dyn std::error::Error + 'static>> {
+
+    fn get_transaction_by_hash(
+        &self,
+        hash: primitives::types::TxHash,
+    ) -> Result<Option<(SignedTransaction, u64)>, Box<dyn std::error::Error + 'static>> {
         let blockchain = self.blockchain.read();
         for (bno, block) in blockchain.iter() {
             for tx in block.body.iter() {
--- pint/src/main.rs
+++ pint/src/main.rs
@@ -1,12 +1,14 @@
 use std::net::{IpAddr, Ipv4Addr};
 
 use clap::Parser;
+use database::mdbx::get_db_path;
 use network::builder::NetworkConfig;
 use node::{builder::LaunchContext, configs::BlockConfig};
-use primitives::{transaction::SignedTransaction, types::Address};
+use primitives::types::Address;
 use tokio::signal;
-use transaction_pool::identifier::TransactionOrigin;
 
+use crate::init::init_txs;
+mod init;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -20,24 +22,26 @@ struct Args {
     #[arg(short, long, default_value_t = 8888)]
     rpc_port: u16,
 
-    #[arg(short, long, default_value_t = String::from("28dcb1338b900419cd613a8fb273ae36e7ec2b1c"))] // pint
+    // Default Miner Address: 28dcb1338b900419cd613a8fb273ae36e7ec2b1c
+    #[arg(short, long, default_value_t = String::from("28dcb1338b900419cd613a8fb273ae36e7ec2b1c"))]
     miner_address: String,
 
-    #[arg(short, long, default_value_t = false)]
+    #[arg(long, default_value_t = false)]
     boot_node: bool,
 
     #[arg(short, long, default_value_t = false)]
     in_memory_db: bool,
 
-    #[arg(short, long, default_value_t = String::from("boot_node"))]                                
+    #[arg(long, default_value_t = false)]
+    test: bool,
+
+    #[arg(long, default_value_t = false)]
+    remove_data: bool,
+
+    #[arg(short, long, default_value_t = String::from("boot_node"))]
     name: String,
 }
 
-// 28dcb1338b900419cd613a8fb273ae36e7ec2b1d pint
-// 0534501c34f5a0f3fa43dc5d78e619be7edfa21a chain
-// 08041f667c366ee714d6cbefe2a8477ad7488f10 apple
-// b2aaaf07a29937c3b833dca1c9659d98a9569070 banana
-// 28dcb1338b900419cd613a8fb273ae36e7ec2b1c
 #[tokio::main]
 async fn main() {
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
@@ -45,19 +49,28 @@ async fn main() {
         unsafe { std::env::set_var("RUST_BACKTRACE", "1") };
     }
 
-    let args = Args::parse(); 
+    let args = Args::parse();
     println!("({}) Try to launch PintChain Node.", args.name);
 
-    let miner_address = Address::from_hex(args.miner_address).expect("Wrong miner address! Node is shut.");
+    if args.remove_data {
+        let pathbuf = get_db_path();
+        if pathbuf.exists() {
+            std::fs::remove_dir_all(&pathbuf).expect("Failed to remove DB directory");
+        }
+        println!("Removing DB data Ok");
+    }
+
+    let miner_address =
+        Address::from_hex(args.miner_address).expect("Wrong miner address! Node is shut.");
 
     let mut network_config = NetworkConfig::new(args.address, args.port, args.rpc_port);
     network_config.boot_node.is_boot_node = args.boot_node;
     let block_config: BlockConfig = BlockConfig::new(miner_address);
-    let launch_context = LaunchContext::new(network_config.clone(), block_config, args.in_memory_db);
-
+    let launch_context =
+        LaunchContext::new(network_config.clone(), block_config, args.in_memory_db);
 
     let node = match launch_context.launch().await {
-        Ok(node ) => node,
+        Ok(node) => node,
         Err(err) => {
             eprintln!("Failed to launch PintChain Node. {:?}", err);
             return;
@@ -66,45 +79,18 @@ async fn main() {
 
     println!("[ Name: {} ] PintChain Node launcing Ok.", args.name);
 
-    if args.boot_node {
-        // test code! initial transactions
-        // pint apple fee 10 value 1000 nonce 0
-        let tx = "0000000000000000000000000000000008041f667c366ee714d6cbefe2a8477ad7488f100000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000003e8e124cac1252a8595c4da5e4d810d231a68571e8b590da337c17a67980e9452ef4e4dbd0a4b7312bd778b5a28dde2e73d152c07a56c5cb246d84f2d6f6d5631aa00";
-        let data = hex::decode(tx).unwrap();
-        let (signed, _) = SignedTransaction::decode(&data).unwrap();
-
-        if let Err(_e) = node.pool.add_transaction(TransactionOrigin::External, signed.into_recovered().unwrap()) {
-            eprintln!("Tx1 add failed");
-        }
-
-        // pint banana fee 10 value 1000 nonce 1
-        let tx = "00000000000000000000000000000001b2aaaf07a29937c3b833dca1c9659d98a95690700000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000003e8c1f3d993c37465ba08cf75eecddb01b214f84d77be915543c47374ae22d4cc6b78354616140743272fd536194a866ad0bd3c6d2d3f4531ee52d3c6bad99b5d1a01";
-        let data = hex::decode(tx).unwrap();
-        let (signed, _) = SignedTransaction::decode(&data).unwrap();
-
-        if let Err(_e) = node.pool.add_transaction(TransactionOrigin::External, signed.into_recovered().unwrap()) {
-            eprintln!("Tx2 add failed");
-        }
-
-        // chain banana fee 5 value 1000 nonce 0
-        let tx = "00000000000000000000000000000000b2aaaf07a29937c3b833dca1c9659d98a95690700000000000000000000000000000000500000000000000000000000000000000000000000000000000000000000003e806cc9be9a58dbba4fa5459512c6d5c3d100bbfcb71cfffb669037243babb0c8678077cba676a8eb659f35a148b551dfadaef085cccbac97729c5a743cab9eec901";
-        let data = hex::decode(tx).unwrap();
-        let (signed, _) = SignedTransaction::decode(&data).unwrap();
-
-        if let Err(_e) = node.pool.add_transaction(TransactionOrigin::External, signed.into_recovered().unwrap()) {
-            eprintln!("Tx3 add failed");
-        }
-
-        node.pool.print_pool();
+    if args.boot_node && args.test {
+        init_txs(&node);
     }
 
     // Starts RPC Server
     // Graceful shutdown
     tokio::select! {
-        _ = node.run_rpc(network_config) => {},
+        _ = node.run_rpc(network_config) => {
+            println!("Rpc Server has been shutdown");
+        },
         _ = signal::ctrl_c() => {
             println!("Ctrl_C: Gracefully shutdown Node..")
         }
     }
-
 }
